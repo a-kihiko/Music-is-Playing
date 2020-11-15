45db767ca697ab207e6acb4c39161f520d700119
a-kihiko <70983201+a-kihiko@users.noreply.github.com>
2020-11-15 10:53:54 -0500

    Update music.js

--- music.js
+++ music.js
@@ -22,22 +22,22 @@ let curr_track = document.createElement('audio');
 
 // Define the tracks that have to be played
 let track_list = [
-  {
+ {
     name: "Night Owl",
     artist: "Broke For Free",
-    image: "https://images.pexels.com/photos/2264753/pexels-photo-2264753.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    image: "https://freemusicarchive.org/image?file=images%2Falbums%2FBroke_For_Free_-_Directionless_EP_-_20110118110857762.png&width=290&height=290&type=image",
     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3"
   },
   {
     name: "Enthusiast",
     artist: "Tours",
-    image: "https://images.pexels.com/photos/3100835/pexels-photo-3100835.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    image: "https://freemusicarchive.org/image?file=images%2Falbums%2FTours_-_Enthusiast_-_20120822134030577.png&width=290&height=290&type=image",
     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3"
   },
   {
     name: "Shipping Lanes",
     artist: "Chad Crouch",
-    image: "https://images.pexels.com/photos/1717969/pexels-photo-1717969.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250&w=250",
+    image: "https://freemusicarchive.org/image?file=images%2Falbums%2FChad_Crouch_-_Arps_-_20190913144052757.jpg&width=290&height=290&type=image",
     path: "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3",
   },
    {
